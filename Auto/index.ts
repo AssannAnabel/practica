627b919b38c24f9031db37553f0328bc428be820
Anabel <assannanabel@gmail.com>
2023-03-30 12:41:41 -0300

    modificado

---
+++ Auto/index.ts
@@ -0,0 +1,10 @@
+import { RegistroAuto } from "./registroAuto";
+import { Auto } from "./autos";
+let vendedor = new RegistroAuto();
+let suran = new Auto("Suran", "Renault", 2016, "Rojo", "Sedan", 59004);
+let corolla= new Auto("Corolla", "Toyota", 2011, "Gris", "Sedan", 202456);
+
+//vendedor.consultarAuto("k"); //CONSULTA POR UN AUTO ESPECIFICO
+//vendedor.darAlta(corolla); // agrega un nuevo auto
+//vendedor.darBaja("207"); //Elimina un vehiculo y devuelve el listado modificado
+
---
+++ Auto/registroAuto.ts
@@ -0,0 +1,50 @@
+/*consultar, borrar,actualizar,y dar de alta*/
+import { Auto } from "./autos";
+import fs from "fs";
+
+export class RegistroAuto {
+
+  data() { return JSON.parse(fs.readFileSync("./listado.json", "utf-8"))}
+
+   consultarListadoAutos() {
+    console.log(this.data())
+  }
+
+  darAlta(auto: Auto,) {
+    
+    let car = [...this.data(), auto];
+    fs.writeFileSync("./listado.json", JSON.stringify(car, null, 2));  
+    console.log(this.data());
+      
+  }
+
+  darBaja(modelo: string) {
+    let listado= this.data(); 
+    let autoBaja = listado.findIndex((listado:{modelo:string}) => listado.modelo === modelo);      
+
+   if (autoBaja >= 0) {
+      listado.splice(autoBaja, 1);
+      console.log("El vehiculo ", modelo, "se dio de baja");
+      fs.writeFileSync("./listado.json", JSON.stringify(listado));
+      console.log(this.data());    
+    } else {
+      console.log(
+        "El vehiculo ",modelo ," no pudo darse de baja porque no se encontro"
+      );
+    }
+  }
+
+  consultarAuto(modelo: string) {
+    let autoEncontrado = this.data().find((auto:{modelo:string}) => auto.modelo === modelo);
+      if (autoEncontrado) {
+      console.log(modelo, " Esta disponible ", autoEncontrado);
+      return autoEncontrado;
+    } else {
+      console.log(modelo, " No esta disponible");
+    }
+  }
+}
+
+
+
+
